ae37ec2d6101171ee540b5423444921be3e36506
aymaneljerari <m.aymane.eljerari@gmail.com>
2022-06-08 12:37:48 +0100

    dataloader stock index fix

--- dataloader.py
+++ dataloader.py
@@ -7,7 +7,7 @@ from datetime import datetime, timedelta
 
 
 class JPXData(Dataset):
-    def __init__(self, batch_size, data_dir="modified_data", stock_list="tokenized_stock_list.csv", stock_prices="stock_prices.csv"):
+    def __init__(self, data_dir="modified_data", stock_list="tokenized_stock_list.csv", stock_prices="stock_prices.csv"):
         """
 			data_dir        (string): Directory with where data is.
 			stock_list      (string): File containing information about each stock.
@@ -16,8 +16,9 @@ class JPXData(Dataset):
         self.data_dir           = data_dir
         self.stock_list         = pd.read_csv(data_dir + '/' + stock_list)
         self.stock_prices       = pd.read_csv(data_dir + '/' + stock_prices)
-        self.batch_size         = batch_size
-        
+
+        self.stock_id = sorted(list(set(self.stock_prices["SecuritiesCode"])))
+
         # Sort dates chronologically and store them in self.__dates
         dates = []
         for i in list(set(self.stock_prices["Date"])):
@@ -26,72 +27,48 @@ class JPXData(Dataset):
     
         self.__dates            = dates
         self.__num_stocks       = len(self.stock_list)
+        self.__indices          = {i: self.stock_id.index(i) for i in self.stock_id}
     
     def __len__(self):
         # Verify data set size
-        return len(self.stock_prices) // self.batch_size
-
+        return len(self.__dates) - 1
+        
     def __getitem__(self, idx):
         # JPX Holiday Calendar 2017 [https://stock-market-holidays.org/2017-japan-exchange-group-holidays/]
 
         # Find the day corresponding to input idx 
         input_date  = self.__dates[idx]
-        target_date = self.__dates[idx+1]
+        target_date = self.__dates[idx+1] 
 
         # Find index of first occurence of input_date in dataset
-        input_date_first = list(self.stock_prices["Date"]).index(input_date.strftime("%Y-%m-%d"))
-
-        # Find index of the last occurence of input_date in dataset
-        reversed_data   = list(self.stock_prices["Date"])[::-1].index(input_date.strftime("%Y-%m-%d"))
-        input_date_last = self.__len__() - 1 - reversed_data
+        input_date_first = input_date.strftime("%Y-%m-%d")
 
         # Find index of first occurence of target_date in dataset
-        target_date_first = list(self.stock_prices["Date"]).index(target_date.strftime("%Y-%m-%d"))
+        target_date_first = target_date.strftime("%Y-%m-%d")
 
-        # Find index of the last occurence of target_date in dataset
-        reversed_data    = list(self.stock_prices["Date"])[::-1].index(target_date.strftime("%Y-%m-%d"))
-        target_date_last = self.__len__() - 1 - reversed_data
+        # Load Date specific data sorted by Securities Code
+        input_data  = self.stock_prices.query("Date == @input_date_first").sort_values(by='SecuritiesCode')[["Open", 'SecuritiesCode']].reset_index()
+        target_data = self.stock_prices.query("Date == @target_date_first").sort_values(by='SecuritiesCode')[["Open", 'SecuritiesCode']].reset_index()
 
-        # print(f"IDX {idx} - Range of input  {input_date_first, input_date_last}, Number of Stocks on day {input_date}: {input_date_last - input_date_first + 1}")
-        # print(f"IDX {idx} - Range of target {target_date_first, target_date_last}, Number of Stocks on day {target_date}: {target_date_last - target_date_first + 1}")
+        # Initialize tensors of zeros
+        input_tensor  = torch.zeros(2000)
+        target_tensor = torch.zeros(2000)
 
-        input_data  = self.stock_prices[["Open"]][input_date_first:input_date_last+1].values.tolist()
-        target_data = self.stock_prices[["Open"]][target_date_first:target_date_last+1].values.tolist()
-        
-        input_tensor  = torch.tensor([x for xs in input_data for x in xs], dtype=torch.float)
-        target_tensor = torch.tensor([x for xs in target_data for x in xs], dtype=torch.float)
+        # Write "Open" price data at it's given stock index
+        for i in range(len(input_data)):
+            input_index = self.__indices[input_data["SecuritiesCode"][i]]
+            input_tensor[input_index] = input_data["Open"][i]
+                
+        for i in range(len(target_data)):
+            target_index = self.__indices[target_data["SecuritiesCode"][i]]
+            target_tensor[target_index] = target_data["Open"][i]
+    
+        # Convert nan to 0
+        input_final  = input_tensor.nan_to_num(nan=0)
+        target_final = target_tensor.nan_to_num(nan=0)
 
-        # Initialize dataframes of zeroes to append to input and target data
-        input_zero_df  = torch.zeros((2000 - len(input_data),), dtype=torch.float)
-        target_zero_df = torch.zeros((2000 - len(target_data),), dtype=torch.float)
-
-        input_concat  = torch.nan_to_num(torch.cat((input_tensor, input_zero_df)), nan=0)
-        target_concat = torch.nan_to_num(torch.cat((target_tensor, target_zero_df)), nan=0)
-
-        input_final  = (input_concat - torch.mean(input_concat)) / torch.std(input_concat)
-        target_final = (target_concat - torch.mean(target_concat)) / torch.std(target_concat)
-
-        # print(f"Input shape: {input_concat}")
-        # print(f"Target shape: {target_concat}")
-
-
-        # input_stacked  = torch.hstack((input_data[:, 0],input_data[:, 1]))
-        # target_stacked = torch.hstack((target_data[:, 0], target_data[:, 1]))
-
-        # print(f"Input shape: {input_stacked}")
-        # print(f"Target shape: {target_stacked}")
-        
-        # # print(f"Input shape: {input_zero_df.shape}")
-        # # print(f"Target shape: {target_zero_df.shape}")
-
-        # input_concat  = pd.concat([input_data, input_zero_df], ignore_index=True)
-        # target_concat = pd.concat([target_data, target_zero_df], ignore_index=True)
-
-        # input_final  = input_concat.stack().values
-        # target_final = target_concat.stack().values
-
-        # print(f"Input shape: {input_final.shape}")
-        # print(f"Target shape: {target_final.shape}")
-
+        # Normalize
+        input_final  = (input_final - torch.mean(input_final)) / torch.std(input_final)
+        target_final = (target_final - torch.mean(target_final)) / torch.std(target_final)
 
         return input_final, target_final
